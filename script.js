8ac500eee0bd021aba862debddd825e8dd25c3c4
Piyush Kumar Yadav <73697893+piyush1212@users.noreply.github.com>
2024-03-24 14:17:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstWord(s) {
-  // your code here
+  // If the input string is empty or doesn't contain any space, return the entire string
+  if (s === '' || !s.includes(' ')) {
+      return s;
+  }
+  
+  // Find the index of the first space
+  const spaceIndex = s.indexOf(' ');
+  
+  // Return the substring from the beginning up to the first space
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
