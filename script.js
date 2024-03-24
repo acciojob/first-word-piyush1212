b7481609929b36dddd2bae473763676b0628b91b
Piyush Kumar Yadav <73697893+piyush1212@users.noreply.github.com>
2024-03-24 14:49:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function firstWord(s) {
     // If the input string is empty or does not contain any space, return the entire string
-    if (!s || !s.includes(" ")) {
-        return s;
+    if (!s || !s.trim().includes(' ')) {
+        return s.trim();
     }
     
     // Split the string by space and return the first element (first word)
-    return s.split(' ')[0];
+    return s.trim().split(' ')[0];
 }
 
 // Prompting user for input
